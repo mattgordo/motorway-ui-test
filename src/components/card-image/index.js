1b8a9b5c687a03ff7f4b1fcc8dc483640394f177
Matt Gordon <dev@mattgordo.com>
2021-01-26 00:41:43 +0000

    Add Modal solution

--- src/components/card-image/index.js
+++ src/components/card-image/index.js
@@ -1,19 +1,44 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Fragment, useState, useCallback } from 'react';
 
 import './card-image.scss';
 
-const CardImage = ({ src, alt, theme }) => {
+const CardImage = ({ src, alt, description, theme }) => {
+
+	const [modalOpen, toggleModal] = useState(false);
+
+	const modalClickHandler = useCallback(e => {
+		e?.preventDefault();
+
+		toggleModal(!modalOpen);
+
+		// Prevent Scroll on Modal Open
+		if (modalOpen) {
+			document.body.style.overflow = 'auto';
+		} else {
+			document.body.style.overflow = 'hidden';
+		}
+	}, [modalOpen]);
 
 	if (!src) {
 		return null;
 	}
 
-	return <div className={`card-image-container card-${theme}`}>
-		<a href={`${src}.jpg`} title="See full image" target="_blank" rel="noreferrer">
-			<img className="card-image" src={src} alt={alt} />
-		</a>
-	</div>;
+	return <Fragment>
+		<div className={`card-image-container card-${theme}`} onClick={modalClickHandler}>
+			<a href={`${src}.jpg`} title="See full image" target="_blank" rel="noreferrer">
+				<img className="card-image" src={src} alt={alt} />
+			</a>
+		</div>
+		<div className={`modal ${modalOpen ? 'open' : ''}`}>
+			<div className="modal-content">
+				<span className="close" onClick={modalClickHandler} aria-label="Close Modal">×</span>
+				<img className="modal-card-image" src={src} alt={alt} />
+				{description && <p className="modal-card-description">{description}</p>}
+			</div>
+		</div>
+	</Fragment>;
+
 };
 
 CardImage.defaultProps = {
@@ -23,6 +48,7 @@ CardImage.defaultProps = {
 CardImage.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string,
+  description: PropTypes.string,
   theme: PropTypes.string
 }
 
